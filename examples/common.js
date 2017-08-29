881a2928a3ce4be6348894afeecbe70c5e277c44
Andrew Throener <athroener@mystartupsolutions.com>
2017-08-29 16:27:08 -0500

    Update commonjs example

--- examples/common.js
+++ examples/common.js
@@ -1,6 +1,7 @@
-var lib = require("../lib");
+// you would use: var PayPalRestApi = require("paypal-rest-api").PayPalRestApi;
+var PayPalRestApi = require("../lib").PayPalRestApi;
 
-const paypal = new lib.PayPalRestApi({
+const paypal = new PayPalRestApi({
     client_id: "ARkR7soWd2kUxFCNPHOmyb3IQhOwiL-wYhRmsRRD1SdslE0u-lCEps4LdN_KocpyEPgaWJXcsFuwd99M",
     client_secret: "ECSQrtNCk09UyKoHfSWuogfaQRmjbgVy9Mg7nc6JOI48z_dMfNonz-3Z3KFCLeX5qhFLGJ9e--DY59gV",
     mode: "sandbox",
